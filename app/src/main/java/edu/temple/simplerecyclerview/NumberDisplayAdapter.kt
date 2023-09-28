0d3d0b68fb25d1640444f5dd7710b5ed9f936831
Margaret Hanley <tuj92824@temple.edu>
2023-09-28 14:28:59 -0400

    refactor onCreateViewHolder

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -19,9 +19,9 @@ class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adap
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-
-        val layout = LayoutInflater.from(parent.context).inflate(R.layout.textview_layout, parent, false)
-        return NumberViewHolder(layout)
+        return NumberViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.textview_layout, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
